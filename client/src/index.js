19d3008e6502c8e27d95169af41dea566c8e8b56
Molly McCarron <mccarronmollye@gmail.com>
2019-05-06 19:15:23 -0400

    Added thunk to app.

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,13 @@ import App from './components/App';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
+import rootReducer from './reducers'
 
+let store = createStore(rootReducer, applyMiddleware(thunk))
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+document.getElementById('root')
+);
---
+++ client/src/reducers/logReducer.js
@@ -0,0 +1,6 @@
+export default (state = {log: [], loading: false}, action) => {
+  switch (action.type){
+    default:
+      return state;
+  }
+}
