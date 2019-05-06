2c1455fe7b808e96443da4070d0154646c3634e7
Molly McCarron <mccarronmollye@gmail.com>
2019-05-06 12:04:29 -0400

    installed react-redux & redux began implementing redux

--- client/src/components/logs/CardContainer.js
+++ client/src/components/logs/CardContainer.js
@@ -1,18 +1,35 @@
 import React, { Component } from 'react'
 import Cards from './Cards'
 import CardInput from './CardInput'
+import { connect } from 'react-redux'
 
 class CardContainer extends Component {
   render(){
     return(
     <div>
-      <CardInput/>
-      <h3>Code Logs</h3>
-      <Cards/>
+      <div className="daily-code-log" >
+        <h1>Daily Code Log</h1>
+      </div>
+      <div className="card-input">
+        <CardInput addCodeLog={this.props.addCodeLog}/>
+      </div>
+      <div className="code-logs" >
+        <h3>Code Logs</h3>
+      </div>
+      <div>
+        <Cards
+          codeLogs={this.props.codeLogs}
+          />
+      </div>
     </div>
     )
   }
 }
 
+const mapStateToProps = state => ({ codeLogs: state.codeLogs })
+
+// const mapDispatchToProps = dispatch => ({
+//   addCodeLog:
+// })
 
 export default CardContainer
--- client/src/components/logs/CardInput.js
+++ client/src/components/logs/CardInput.js
@@ -17,6 +17,23 @@ class CardInput extends Component{
     }
   }
 
+  handleOnChange(event){
+    this.setState({
+      title: event.target.value,
+      date: event.target.value,
+      learned: event.target.value,
+      unsure: event.target.value,
+      goals_accomplished: event.target.value,
+      goals_tomorrow: event.target.value,
+      mood: event.target.value,
+    })
+  }
+
+  handleOnSubmit(event){
+    event.preventDefault();
+    this.props.addCodeLog(this)
+  }
+
 
   render(){
     return(
@@ -25,45 +42,85 @@ class CardInput extends Component{
           <form onSubmit={(event) => this.handleOnSubmit(event)}>
               <div className="field">
                 <label>Title:</label>
-                <input type="text" placeholder="Day 2 of #100DaysOfCode"/>
+                <input
+                  name="title"
+                  type="text"
+                  value={this.state.title}
+                  onChange={(event) => this.handleOnChange(event)}
+                  placeholder="Day 2 of #100DaysOfCode"
+                />
               </div>
               <div className="field">
                 <label>Date:</label>
-                <input type="text" placeholder="01/01/19"/>
+                <input
+                  name="date"
+                  type="text"
+                  value={this.state.date}
+                  onChange={(event) => this.handleOnChange(event)}
+                  placeholder="01/01/19"
+                  />
               </div>
               <div className="field">
                 <label>Goals I accomplished today:</label>
-                <textarea rows="2" placeholder="Built form for personal project."/>
+                <textarea
+                  name="goals_accomplished"
+                  rows="2"
+                  value={this.state.goals_accomplished}
+                  onChange={(event) => this.handleOnChange(event)}
+                  placeholder="Built form for personal project."
+                  />
               </div>
               <div className="field">
                 <label>What I learned today:</label>
-                <textarea rows="2" placeholder="Properly organizing Component files in React"/>
+                <textarea
+                  name="learned"
+                  rows="2"
+                  value={this.state.learned}
+                  onChange={(event) => this.handleOnChange(event)}
+                  placeholder="Properly organizing Component files in React"
+                />
               </div>
               <div className="field">
                 <label>What I don't understand:</label>
-                <textarea rows="2" placeholder="How user input on the front end (React) will link to Rails backend of project."/>
+                <textarea
+                  name="unsure"
+                  rows="2"
+                  value={this.state.unsure}
+                  onChange={(event) => this.handleOnChange(event)}
+                  placeholder="How user input on the front end (React) will link to Rails backend of project."
+                  />
               </div>
               <div className="field">
                 <label>Goals for tomorrow:</label>
-                <textarea rows="2" placeholder="Persist user input to the database."/>
+                <textarea
+                  name="goals_tomorrow"
+                  rows="2"
+                  value={this.state.goals_tomorrow}
+                  onChange={(event) => this.handleOnChange(event)}
+                  placeholder="Persist user input to the database."
+                  />
               </div>
               <div className="field">
                 <label>How I feel about my coding progress today:</label>
-                <div>
-                  <div className="ui radio checkbox">
-                      <input type="radio" name="mood"/>
-                      <label><i className="large smile outline icon"></i></label>
-                  </div>
+                <div className="ui radio checkbox space">
+                    <input
+                      type="radio"
+                      name="smile"
+                      />
+                    <label><i className="big smile outline icon"/></label>
                 </div>
-                <div>
-                  <div className="ui radio checkbox">
-                      <input type="radio" name="mood"/>
-                      <label><i className="large meh outline icon"></i></label>
-                  </div>
+                <div className="ui radio checkbox space">
+                    <input
+                      type="radio"
+                      name="mwh"
+                      />
+                    <label><i className="big meh outline icon"/></label>
                 </div>
-                <div className="ui radio checkbox">
-                    <input type="radio" name="mood"/>
-                    <label><i className="large frown outline icon"></i></label>
+                <div className="ui radio checkbox space">
+                    <input
+                      type="radio"
+                      name="frown"/>
+                    <label><i className="big frown outline icon"/></label>
                 </div>
               </div>
             <input type="submit"/>
