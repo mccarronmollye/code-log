8f1f272f018e41e4f2b97f81ccd438ca082a92d3
Molly McCarron <mccarronmollye@gmail.com>
2019-05-07 11:10:44 -0400

    Added mood to card.

--- client/src/components/Cards.js
+++ client/src/components/Cards.js
@@ -15,20 +15,17 @@ class Cards extends Component {
           <div className="ui cards">
             <div className="card" key={log.id}>
               <div className="content">
-                <p>Date</p>
+                <p>{log.date}</p>
                 <h4 id="title">{log.title}</h4>
                   <ul>
-                    <li>Goals I accomplished today:</li>
-                    <li>What I learned:</li>
-                    <li>What I don't understand:</li>
-                    <li>Goals for tomorrow:</li>
+                    <li>Goals I accomplished today: {log.goals_accomplished}</li>
+                    <li>What I learned: {log.learned}</li>
+                    <li>What I don't understand: {log.unsure}</li>
+                    <li>Goals for tomorrow: {log.goals_tomorrow}</li>
                   </ul>
               </div>
-
               <div className="feelings">
-                <i className="smile outline icon"></i>
-                <i className="meh outline icon"></i>
-                <i className="frown outline icon"></i>
+                <i className={`${log.mood} outline icon`}></i>
               </div>
             </div>
           </div>
@@ -40,48 +37,6 @@ class Cards extends Component {
   render(){
     return <div className="ui relaxed divided list">{this.renderList()}</div>
   }
-
-
-
-
-
-
-
-
-
-
-
-  // render() {
-  //   const logs = this.props.logs.map((log, i) => <li key={i}>{log.title}</li>)
-  //
-  //   return (
-      // <div className="card-container">
-      //   <div className="ui cards">
-      //     <div className="card">
-      //       <div className="content">
-      //         <p>Date</p>
-      //         <h4 id="title">Title</h4>
-      //           <ul>
-      //             <li>Goals I accomplished today:</li>
-      //             <li>What I learned:</li>
-      //             <li>What I don't understand:</li>
-      //             <li>Goals for tomorrow:</li>
-      //           </ul>
-      //           <h3>Logs</h3>
-      //           <ul>
-      //             { this.props.loading ? <li>loading...</li> : logs }
-      //           </ul>
-      //       </div>
-      //       <div className="feelings">
-      //         <i className="smile outline icon"></i>
-      //         <i className="meh outline icon"></i>
-      //         <i className="frown outline icon"></i>
-      //       </div>
-      //     </div>
-      //   </div>
-      // </div>
-  //   )
-  // }
 
 }
 
